db64865613de1d596ab0d3699f0700327704e7b3
User-XYZ <nwosu4686@gmail.com>
2023-10-02 22:23:29 +0100

    created about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <main>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quaerat non accusamus ut hic consequuntur fugiat incidunt. Perferendis ducimus fuga sit ut, pariatur, accusantium placeat recusandae repudiandae minima quas modi?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quaerat non accusamus ut hic consequuntur fugiat incidunt. Perferendis ducimus fuga sit ut, pariatur, accusantium placeat recusandae repudiandae minima quas modi?</p>
+    </main>
+  )
+}
+
+export default About
