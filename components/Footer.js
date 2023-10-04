243ed23c7d2a1f0f43d78fa41f2cda0ef734d26d
User-XYZ <nwosu4686@gmail.com>
2023-10-04 18:25:43 +0100

    created Navbar & Footer components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright EPL Clubs 2023</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <p>EPL Clubs</p>
+            <a>Home</a>
+            <a>About</a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
