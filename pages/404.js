a70c43a2204272fcad3694cff5d369e910044a93
User-XYZ <nwosu4686@gmail.com>
2023-10-05 21:49:24 +0100

    modified 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const NotFound = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
+
     return (
         <main>
             <h1>Ooops.........page not found!</h1>
