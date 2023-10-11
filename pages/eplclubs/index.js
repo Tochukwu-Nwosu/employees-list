023ea768c38a8b516a539f6a639fefd1b4c7b654
User-XYZ <nwosu4686@gmail.com>
2023-10-11 23:00:57 +0100

    linked employees list

--- pages/eplclubs/index.js
+++ pages/eplclubs/index.js
@@ -25,7 +25,7 @@ const Clubs = ({ employees }) => {
                     {employees.map(employee => {
                         return (
                             <div key={employee.id}>
-                                <Link href={'/'}>{employee.name}</Link>
+                                <Link href={'/' + employee.id}>{employee.name}</Link>
                             </div>
                         )
                     })}
