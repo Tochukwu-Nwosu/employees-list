93976a832a79a639272b9ef2d95cd63989d5732f
User-XYZ <nwosu4686@gmail.com>
2023-10-13 22:24:39 +0100

    modified navbar and footer style

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <div className="layout-container">
             <Navbar />
             { children}
             <Footer />
-        </>
+        </div>
     );
 }
  
